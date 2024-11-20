130f0b228b7d14b4060344527e7eef77280b3df9
Mauricio González <hello@mauro-gonzalez.dev>
2024-11-20 13:31:26 -0300

    test 22

--- netlify/edge-functions/get-products.ts
+++ netlify/edge-functions/get-products.ts
@@ -1,5 +1,5 @@
 export default async () => {
-  return new Response('Hola Mundo', {
+  return new Response('Hola Mundo desde get products', {
     headers: { 'content-type': 'text/plain' },
   });
 };
